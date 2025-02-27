a2dcccd45cf2c80d588e9f89e303dc617b764df5
SHENG HUANG <shenghuang147@gmail.com>
2025-02-27 16:51:22 +0000

    fix replace_package work path

--- replace_package.sh
+++ replace_package.sh
@@ -1,11 +1,11 @@
 # 解决自添加源同名包无法覆盖官方源中的包
 
-M_PKG_LIST=$(find ./feeds/myPackages -type f -name "Makefile" -printf "%h\n" | sort -u)
+M_PKG_LIST=$(find ./openwrt/feeds/myPackages -type f -name "Makefile" -printf "%h\n" | sort -u)
 
 for pkg_path in $M_PKG_LIST; do
 	pkg_name=$(echo $pkg_path |awk -F / '{print $NF}')
 	# 官方同名包路径
-	wrt_pkg_path=$(find ./feeds -type d ! -path '*/myPackages/*' -name $pkg_name \
+	wrt_pkg_path=$(find ./openwrt/feeds -type d ! -path '*/myPackages/*' -name $pkg_name \
 		-exec test -f "{}/Makefile" \; -print)
 	[ -n "$wrt_pkg_path" ] || continue
 
